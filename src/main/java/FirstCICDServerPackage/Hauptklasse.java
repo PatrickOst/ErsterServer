2ca7593c709f15eec443e9bcc16590464df451fb
Patrick Gruenenfelder <patrick.gruenenefelder@ost.ch>
2021-06-08 14:42:51 +0200

    Commit Part 3 Erste Socket Connection (nur Single Connection)

--- src/main/java/FirstCICDServerPackage/Hauptklasse.java
+++ src/main/java/FirstCICDServerPackage/Hauptklasse.java
@@ -4,8 +4,12 @@ import FirstCICDServerPackage.config.Configuration;
 import FirstCICDServerPackage.config.ConfigurationManager;
 import com.sun.net.httpserver.*;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.lang.System;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
 
 
 public class Hauptklasse {
@@ -25,6 +29,34 @@ public class Hauptklasse {
         System.out.println("Using Port: " + conf.getPort());
         System.out.println("Using WebRoot: " + conf.getWebroot());
 
+        try {
+            ServerSocket serverSocket = new ServerSocket(conf.getPort());
+            Socket socket = serverSocket.accept();
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            String html = "<html><head><title>Simple Java HTTP Server</title></head><body><h1>This page was served using my Simple Java HTTP Server</h1></body></html>";
+
+            final String CRLF = "\n\r";
+            String response =
+                    "HTTP/1.1 200 OK" + CRLF + // Status Line : HTTP Version Response_Code Response_Message
+                            "Content-Length: " + html.getBytes().length + CRLF + // Header
+                            CRLF +
+                            html +
+                            CRLF + CRLF;
+
+            outputStream.write(response.getBytes());
+
+
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            serverSocket.close();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
 
