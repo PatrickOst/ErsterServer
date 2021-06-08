9f28caa0c6a543eb864d14d183fab99a02e5dfb6
Patrick Gruenenfelder <patrick.gruenenefelder@ost.ch>
2021-06-08 11:19:25 +0200

    Commit before Server implementation

--- src/main/java/FirstCICDServerPackage/Hauptklasse.java
+++ src/main/java/FirstCICDServerPackage/Hauptklasse.java
@@ -5,7 +5,7 @@ import java.lang.System;
 public class Hauptklasse {
 
     public static void main(String[] args) {
-        System.out.println("---Main-gestartet---");
+        System.out.println("---Main-wurde-gestartet---");
         // Erstes Objekt erstellen
         TestKlasse1 TK1 = new TestKlasse1("Patrick", 1985);
         System.out.println("Name = " + TK1.getmName());
